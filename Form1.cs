6c20a4b0ed1c3001a850528ed72a6b35819bb003
Tino <Konstantin.v.milchev@gmail.com>
2023-10-25 08:18:18 +0100

    Adding selection and removal of date functionality

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,25 @@ namespace WinForms.Invoice.Generator
             listBox.DisplayMember = "Item"; 
             listBox.ValueMember = "Item"; 
             listBox.DataSource = _bindingSource;
+            listBox.MouseDown += ListBox_MouseDown;
+            var contextMenu = new ContextMenuStrip();
+            listBox.ContextMenuStrip = contextMenu;
+            contextMenu.Items.Add("Delete", null, ContextMenuItem_Click);
+        }
+
+        private void ContextMenuItem_Click(object? sender, EventArgs e)
+        {
+            _bindingList.RemoveAt(listBox.SelectedIndex);
+        }
+
+        private void ListBox_MouseDown(object? sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                // Show the context menu at the cursor's location
+                ListBox listBox = (ListBox)sender;
+                listBox.ContextMenuStrip.Show(listBox, e.Location);
+            }
         }
 
         private void calendar_DateSelected(object sender, DateRangeEventArgs e)
